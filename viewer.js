584a2c5674944560ce75cef08ec14465d93e592e
syottu <haruto.kunsan@akane.waseda.jp>
2024-10-23 12:42:32 +0900

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -1,27 +1,8 @@
-async function getAccessToken(callback) {
-    try {
-        // AWS API Gatewayのエンドポイントを使用してトークンを取得
-        const resp = await fetch('https://szdv06tuba.execute-api.us-east-1.amazonaws.com/default/ForgeTokenFunction');
-        if (!resp.ok) {
-            throw new Error(await resp.text());
-        }
-        const { access_token, expires_in } = await resp.json();
-        callback(access_token, expires_in);
-    } catch (err) {
-        alert('Could not obtain access token. See the console for more details.');
-        console.error(err);
-    }
-}
-
 export function initViewer(container) {
     return new Promise(function (resolve, reject) {
         Autodesk.Viewing.Initializer({ env: 'AutodeskProduction', getAccessToken }, function () {
-            const config = {
-                extensions: ['Autodesk.DocumentBrowser']
-            };
-            const viewer = new Autodesk.Viewing.GuiViewer3D(container, config);
+            const viewer = new Autodesk.Viewing.GuiViewer3D(container);
             viewer.start();
-            viewer.setTheme('light-theme');
             resolve(viewer);
         });
     });
@@ -29,13 +10,12 @@ export function initViewer(container) {
 
 export function loadModel(viewer, urn) {
     return new Promise(function (resolve, reject) {
-        function onDocumentLoadSuccess(doc) {
-            resolve(viewer.loadDocumentNode(doc, doc.getRoot().getDefaultGeometry()));
-        }
-        function onDocumentLoadFailure(code, message, errors) {
-            reject({ code, message, errors });
-        }
-        viewer.setLightPreset(0);
-        Autodesk.Viewing.Document.load('urn:' + urn, onDocumentLoadSuccess, onDocumentLoadFailure);
+        Autodesk.Viewing.Document.load(`urn:${urn}`, function (doc) {
+            const defaultModel = doc.getRoot().getDefaultGeometry();
+            viewer.loadDocumentNode(doc, defaultModel);
+            resolve(viewer);
+        }, function (err) {
+            reject(err);
+        });
     });
 }
